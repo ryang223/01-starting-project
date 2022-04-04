672e84431a7fe1bbe49435416da89d7fcb0d8a08
ryang223 <ryangoodwd@gmail.com>
2022-04-04 17:35:33 -0400

    portal works properly and sources HTML element

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import Wrapper from '../Helpers/Wrapper'
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
@@ -43,7 +44,7 @@ const AddUser = (props) => {
     };
 
     return (
-        <div>
+        <Wrapper>
             {error && 
                 <ErrorModal 
                 title={error.title} 
@@ -68,7 +69,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 };
 
